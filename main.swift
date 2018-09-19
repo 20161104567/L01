57b83cc288c1f0e3ae9908c79fdacce97a846bf1
20161104567 <846129391@qq.com>
2018-09-19 11:21:22 +0800

    练习
    练习

---
+++ main.swift
@@ -0,0 +1,37 @@
+//
+//  main.swift
+//  L01
+//
+//  Created by 20161104567 on 2018/9/5.
+//  Copyright © 2018年 20161104567. All rights reserved.
+//
+
+import Foundation
+var a=1  //变量
+a=10
+var b=2
+let c=a+b  //😋常量
+print(c)
+
+var str = "Hello "
+var s:String = "World "   //指明类型
+var t:Int = 100
+print(s,t)
+
+var str1 = "Li"
+str1 = str1 + "Huimin"  //字符串之间相加连
+str1 = "\(str1),女,\(20)"   //所有类型相连
+print(str1)
+
+var str2 = "XiaoMin"
+var i = 23
+str2 = "\(str2),女,\(i)"
+print(str2)
+
+var arr = ["I","am","1"]
+var arr1 = [1,2,3,4,5]
+var arr2:[Int] = []
+//var arr3 = []
+//var arr4 = string[]()
+print(arr,arr1,arr2)
+
